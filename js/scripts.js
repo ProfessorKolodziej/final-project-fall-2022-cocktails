88c241278f05bc62457aee38c4466b91f3a3ab06
oliviahur <ohur@bu.edu>
2023-12-15 16:01:27 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -6,64 +6,118 @@
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
 
-// PERSONAL ACCESS TOKEN : patt85hn5qn6NSDm3.ffadbcdda546c381ce054eba74e69d5909c3daa42aee0eeef18438c5c3786ee2
-
-// https://airtable.com/appmqx3AsOPOuAddI/tblIDzvpWZes5YcMG/viw2juumuVu1243aB/recY3eLA13afIWRZS?blocks=hide
-
-// const API_KEY =
-// 'patt85hn5qn6NSDm3.ffadbcdda546c381ce054eba74e69d5909c3daa42aee0eeef18438c5c3786ee2';
-
-// const baseId = 'appmqx3AsOPOuAddI/tblIDzvpWZes5YcMG';
-// const tableId = 'tblIDzvpWZes5YcMG/viw2juumuVu1243aB'
-// const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableId}`;
-
-// Try putting ids on these things like page-title and page-choices so that in your JS,
-// you can use document.querySelector('#page-title') to get at this easily, and then set
-// the inner text to whatever the next page should be when you click the next button.
-
-
 
 let pageNumber = 0;
-// [{name: "blue lagoon", flavors: ["lemon", "mint"]}]
-// list.filter((drink) => {})
-let formResponses = {
 
-}
+let airData = [];
+let rankedData = [];
 
-async function fetchData () {
-	const API_KEY =
-	'patt85hn5qn6NSDm3.ffadbcdda546c381ce054eba74e69d5909c3daa42aee0eeef18438c5c3786ee2';
+async function fetchData() {
+	return new Promise((resolve, reject) => {
+	  const API_KEY =
+		 'patt85hn5qn6NSDm3.ffadbcdda546c381ce054eba74e69d5909c3daa42aee0eeef18438c5c3786ee2';
 
-	const baseId = 'appmqx3AsOPOuAddI';
-	const tableId = 'tblIDzvpWZes5YcMG';
-	const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableId}`;
+	  const baseId = 'appmqx3AsOPOuAddI';
+	  const tableId = 'tblIDzvpWZes5YcMG';
+	  const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableId}`;
 
-
-	fetch(apiUrl, {
-	headers: {
-		'Authorization': `Bearer ${API_KEY}`
-	}
-	})
-	.then(response => response.json())
-	.then(data => {
-	console.log(data); // This will log the retrieved data from your Airtable base
-	})
-	.catch(error => {
-	console.error('Error fetching data:', error);
+	  fetch(apiUrl, {
+		 headers: {
+			'Authorization': `Bearer ${API_KEY}`
+		 }
+	  })
+		 .then(response => response.json())
+		 .then(data => {
+			airData = data;
+			console.log(airData);
+			console.log(baseChoiceValues);
+			console.log(mixerChoiceValues);
+			console.log(flavorChoiceValues);
+			console.log(sweetnessChoiceValue);
+			resolve(); // Resolve the promise when data is fetched successfully
+		 })
+		 .catch(error => {
+			console.error('Error fetching data:', error);
+			reject(error); // Reject the promise if there is an error
+		 });
 	});
-}
+ }
 
-const airtableDrinks = fetchData();
 
 async function determineDrink() {
-	const lime = airtableDrinks.filter(cocktail => cocktail.fields.Flavors.includes("lime"));
-	console.log("lime: " + lime)
+	await fetchData();
+
+	// obtain list of my items
+	let userSelections = [sweetnessChoiceValue]
+
+	for (const [key, value] of Object.entries(baseChoiceValues)) {
+		if (value) {
+			userSelections.push(key)
+		}
+	}
+	for (const [key, value] of Object.entries(mixerChoiceValues)) {
+		if (value) {
+			userSelections.push(key)
+		}
+	}
+	for (const [key, value] of Object.entries(flavorChoiceValues)) {
+		if (value) {
+			userSelections.push(key)
+		}
+	}
+
+
+	const scoreList = []
+	// iterate over each row in airData (grab one drink at a time)
+	for (let i = 0; i < airData.records.length; i++) {
+		const row = airData.records[i]
+		const name = row.fields.Name
+		const notes = row.fields.Notes
+		const recipe = row.fields.Recipe
+		// keep track of the ingredients/characteristics of this drink
+		let options = [row.fields.Sweetness]
+		options = options.concat(row.fields.Base)
+		options = options.concat(row.fields.Flavors)
+		options = options.concat(row.fields.Mixer)
+
+		// iterate over options, count how many options match our userSelections
+		let count = 0
+		for (let i = 0; i < options.length; i++) {
+			if (userSelections.includes(options[i])) {
+				count++
+			}
+		}
+
+		scoreList.push([count, name, notes, recipe])
+	 }
+
+	console.log("list of scores:", scoreList)
+	// sort scoreList by count of each drink
+	scoreList.sort((a, b) => b[0] - a[0])
+	console.log("sorted:", scoreList)
+
+	const firstFour = scoreList.slice(0,4);
+	let cards = "";
+
+	for (let i = 0; i < 4; i++) {
+		cards +=
+		`
+		<div class="drink-cards">
+			<h2>${firstFour[i][1]}</h2>
+			<p>${firstFour[i][2]}</p>
+			<a href="${firstFour[i][3]}">Recipe</a>
+		</div>
+
+		`
+	}
+
+	potentialDrinks.innerHTML=cards;
 }
 
-//determineDrink();
 
 const title = document.querySelector("#page-title");
-const martini = document.querySelector("#pink-martini")
+const martini = document.querySelector("#pink-martini");
+const champagne = document.querySelector("#champagne-img")
 const nextButton = document.querySelector("#next");
 const prevButton = document.querySelector("#prev");
 const baseChoice = document.querySelector(".base-choice");
@@ -76,9 +130,9 @@ const restartButton = document.querySelector("#restart");
 
 function updateTitles () {
 	if (pageNumber === 0) {
-		title.innerHTML = "WONDERING WHAT YOU SHOULD ORDER AT THE BAR?";
-	} else if (pageNumber === 1) {
 		title.innerHTML = "ARE YOU 21 OR OLDER?";
+	} else if (pageNumber === 1) {
+		title.innerHTML = "WONDERING WHAT YOU SHOULD ORDER AT THE BAR?";
 	} else if (pageNumber === 2) {
 		title.innerHTML = "SORRY, COME BACK ON YOUR 21ST BIRTHDAY!";
 	} else if (pageNumber === 3) {
@@ -95,35 +149,55 @@ function updateTitles () {
 }
 
 function updateContent () {
-	if (pageNumber === 0 || pageNumber === 1 || pageNumber === 2) {
+	if (pageNumber === 0) {
+		martini.style.display = "none";
+		champagne.style.display = "none";
 		baseChoice.style.display = "none";
 		mixerChoice.style.display = "none";
 		flavorChoice.style.display = "none";
 		sweetnessChoice.style.display = "none";
 		potentialDrinks.style.display = "none";
-	} else if (pageNumber === 3) {
-		baseChoice.style.display = "block";
+	} else if (pageNumber === 1) {
+		martini.style.display = "block";
+		champagne.style.display = "none";
+		baseChoice.style.display = "none";
 		mixerChoice.style.display = "none";
 		flavorChoice.style.display = "none";
 		sweetnessChoice.style.display = "none";
 		potentialDrinks.style.display = "none";
+	} else if (pageNumber === 2) {
+		baseChoice.style.display = "none";
+		mixerChoice.style.display = "none";
+		flavorChoice.style.display = "none";
+		sweetnessChoice.style.display = "none";
+		potentialDrinks.style.display = "none";
+		champagne.style.display = "block";
+		martini.style.display = "none";
+	} else if (pageNumber === 3) {
+		baseChoice.style.display = "grid";
+		champagne.style.display = "none";
+		mixerChoice.style.display = "none";
+		flavorChoice.style.display = "none";
+		sweetnessChoice.style.display = "none";
+		potentialDrinks.style.display = "none";
+		martini.style.display = "none";
 	} else if (pageNumber === 4) {
 		baseChoice.style.display = "none";
-		mixerChoice.style.display = "block";
+		mixerChoice.style.display = "grid";
 		flavorChoice.style.display = "none";
 		sweetnessChoice.style.display = "none";
 		potentialDrinks.style.display = "none";
 	} else if (pageNumber === 5) {
 		baseChoice.style.display = "none";
 		mixerChoice.style.display = "none";
-		flavorChoice.style.display = "block";
+		flavorChoice.style.display = "grid";
 		sweetnessChoice.style.display = "none";
 		potentialDrinks.style.display = "none";
 	} else if (pageNumber === 6) {
 		baseChoice.style.display = "none";
 		mixerChoice.style.display = "none";
 		flavorChoice.style.display = "none";
-		sweetnessChoice.style.display = "block";
+		sweetnessChoice.style.display = "flex";
 		potentialDrinks.style.display = "none";
 	} else if (pageNumber === 7) {
 		baseChoice.style.display = "none";
@@ -131,90 +205,129 @@ function updateContent () {
 		flavorChoice.style.display = "none";
 		sweetnessChoice.style.display = "none";
 		potentialDrinks.style.display = "grid";
+		determineDrink();
 	}
 }
 
-// function showCard(cardNumber) {
-//       const card = document.getElementById(`card${cardNumber}`);
-// 		if (card !== "undefined") {
-// 			card.style.display = 'block';
-// 		}
-//     }
-
-// function hideCard(cardNumber) {
-//       const card = document.getElementById(`card${cardNumber}`);
-// 		if (card !== "undefined") {
-// 			card.style.display = 'block';
-// 		}
-//     }
-
-const baseChoiceArea = document.querySelector('.base-choice');
-baseChoiceArea.addEventListener('mouseover', showCard);
-
-function showCard(event) {
-	alert();
-	const hovered = event.target;
-
-	if ( hovered.classList.includes('hover-element')){
-		const card = hovered.querySelector('.card');
-		if (card !== "undefined") {
-			card.style.display = 'block';
+function showCard(cardNumber) {
+      const card = document.getElementById(`card${cardNumber}`);
+		console.log("clicked card", cardNumber, card);
+		if (card !== null && card !== undefined) {
+			card.style.display = "flex";
 		}
+		console.log("updated card", card)
+}
+
+function hideCard(cardNumber) {
+      const card = document.getElementById(`card${cardNumber}`);
+		if (card !== null && card !== undefined) {
+			card.style.display = "none";
+		}
+}
+
+let baseChoiceValues = {};
+let mixerChoiceValues = {};
+let flavorChoiceValues = {};
+let sweetnessChoiceValue = 'none';
+
+function updateChoices(form, checkboxId, isChecked) {
+	switch(form) {
+		case "base":
+			baseChoiceValues[checkboxId] = isChecked;
+			break;
+		case "mixer":
+			mixerChoiceValues[checkboxId] = isChecked;
+			break;
+		case "flavor":
+			flavorChoiceValues[checkboxId] = isChecked;
+			break;
+		case "sweetness":
+			sweetnessChoiceValue = checkboxId;
+			break;
 	}
 }
 
 function updateButtons () {
 	if (pageNumber === 0) {
-		prevButton.style.display = "none";
+		prevButton.style.display = "inline";
 		restartButton.style.display = "none";
-		nextButton.style.display = "block";
+		nextButton.style.display = "inline";
+		nextButton.innerHTML = "Of Course!";
+		prevButton.innerHTML = "Not Yet!";
+	} else if (pageNumber === 1){
+		restartButton.style.display = "none";
+		prevButton.style.display = "none";
+		nextButton.style.display = "inline";
 		nextButton.innerHTML = "Yes!";
-	} else if (pageNumber === 7) {
+		prevButton.innerHTML = "Back";
+	} else if (pageNumber === 2){
+		restartButton.style.display = "none";
+		prevButton.style.display = "none";
+		nextButton.style.display = "none";
+		prevButton.innerHTML = "Back";
+	} else if (pageNumber === 3){
+		restartButton.style.display = "none";
+		prevButton.style.display = "none";
+		nextButton.style.display = "inline";
+		nextButton.innerHTML = "Next";
+	}  else if (pageNumber === 7) {
 		prevButton.style.display = "none";
 		nextButton.style.display = "none"
-		restartButton.style.display = "block";
+		restartButton.style.display = "inline";
+		prevButton.innerHTML = "Back";
 	} else {
-		prevButton.style.display = "block";
-		nextButton.style.display = "block";
+		prevButton.style.display = "inline";
+		nextButton.style.display = "inline";
 		restartButton.style.display = "none";
 		nextButton.innerHTML = "Next";
+		prevButton.innerHTML = "Back";
 	}
 }
 
 function restartPage () {
-	pageNumber = 0;
+	pageNumber = 3;
+	updateTitles();
+	updateContent();
+	updateButtons();
 }
 
 function nextPage() {
-	pageNumber++;
+	if (pageNumber === 0) {
+		pageNumber = 1;
+	} else if (pageNumber === 1) {
+		pageNumber = 3;
+	}	else {
+		pageNumber++;
+	}
+
 	updateTitles();
 	updateContent();
-	updateButtons ();
-	// showCard ();
-	// hideCard ();
+	updateButtons();
+	showCard();
 }
 
 function yesResponse() {
 	pageNumber = 3;
 }
 
-function prevPage(){
-	pageNumber--;
+function prevPage() {
+	if (pageNumber === 0) {
+		pageNumber = 2;
+	} else {
+		pageNumber--;
+	}
+
 	updateTitles();
 	updateContent();
-	updateButtons ();
-	// showCard ();
-	// hideCard ();
+	updateButtons();
+	showCard();
 }
 
 updateTitles();
 updateContent();
 updateButtons();
-// nextPage ();
-// prevPage ();
-// restartPage ();
 
-console.log (pageNumber);
+
 
 
 
